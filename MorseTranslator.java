7cca29476cf1b9539ee249bec4e63f1e637f558c
bfentor <fentorb24@gmail.com>
2024-10-20 01:45:26 +0300

    Optimized translation algo

--- MorseTranslator.java
+++ MorseTranslator.java
@@ -1,4 +1,9 @@
 public class MorseTranslator {
+    final static String[] letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n",
+                                   "o","p","q","r","s","t","u","v","w","x","y","z"};
+    final static String[] morse = {"•-","-•••","-•-•","-••","•","••-•","--•","••••","••",
+                                   "•---","-•-","•-••","--","-•","---","•--•","--•-","•-•",
+                                   "•••","-","••-","•••-","•--","-••-","-•--","--••"};
     public static void main(String[] args) {
         String text = "";
         try {
@@ -7,13 +12,31 @@ public class MorseTranslator {
             System.out.println("Exception encountered");
             System.exit(0);
         }
-        //System.out.println(text);
         for (int i = 0; i < text.length(); i++) {
             System.out.print(getNextAlpha(text.substring(i,i+1)) + " ");
         }
         System.out.println("");
-    }   
+    }
+    // Binary search implementation
     public static String getNextAlpha(String alpha) {
+        int l = 0;
+        int r = letters.length-1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            //System.out.println(mid);
+            if (letters[mid].equals(alpha)) {
+                return morse[mid];
+            } else if (letters[mid].compareTo(alpha) > 0) {
+                r = mid - 1;
+            } else {
+                l = mid +1;
+            }
+        }
+        return "/";
+    }
+    // Previous implementation left here for educational purposes:
+    /* 
+    public static String getNextAlphaDEP(String alpha) {
         switch(alpha) {
             case "a":
                 return "•-";
@@ -72,4 +95,5 @@ public class MorseTranslator {
         }
         return "";
     }
+    */
 }
